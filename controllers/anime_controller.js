00462bcd1a7161a56959703e1426c3f701af6126
Vikash Anand <vaui0906@gmail.com>
2025-01-01 21:01:15 +0530

    commented out pagination feature

--- controllers/anime_controller.js
+++ controllers/anime_controller.js
@@ -31,11 +31,11 @@ export const getAllAnime = async (req, res) => {
   }
 
   // pagination
-  let page = Number(req.query.page) || 1;
-  let limit = Number(req.query.limit) || 10;
-  let skip = (page - 1) * limit;
+  // let page = Number(req.query.page) || 1;
+  // let limit = Number(req.query.limit) || 10;
+  // let skip = (page - 1) * limit;
 
-  apiData = apiData.skip(skip).limit(limit);
+  // apiData = apiData.skip(skip).limit(limit);
 
   try {
     const Anime = await apiData;
