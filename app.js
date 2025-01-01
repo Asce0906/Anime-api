bc44df8e3a79f284a611fba6cc6267b510e71224
Vikash Anand <vaui0906@gmail.com>
2025-01-01 20:56:21 +0530

    placed the cors in correct position

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-import "dotenv/config";
-import cors from "cors";
-import express from "express";
-import anime_route from "./router/anime_route.js";
-import connectDB from "./DB_connection/db_connection.js";
-app.use(cors()); 
+  import "dotenv/config";
+  import cors from "cors";
+  import express from "express";
+  import anime_route from "./router/anime_route.js";
+  import connectDB from "./DB_connection/db_connection.js";
+  
+  const app = express();
+  const PORT = process.env.PORT || 3000;
+  
+  app.use(cors());
+  app.use(express.json());
+  
+  
+  app.get("/", (req, res) => {
+    res.send("I am the devil of my word");
+  });
+  
+  app.use("/api/anime", anime_route);
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+  const start = async () => {
+    try {
+      await connectDB();
+      app.listen(PORT, () => {
+        console.log(`${PORT} is up and running`);
+      });
+    } catch (error) {
+      console.log("Error making connection with DB", error);
+    }
+  };
 
-app.get("/", (req, res) => {
-  res.send("I am the devil of my word");
-});
-
-app.use("/api/anime", anime_route);
-
-const start = async () => {
-  try {
-    await connectDB();
-    app.listen(PORT, () => {
-      console.log(`${PORT} is up and running`);
-    });
-  } catch (error) {
-    console.log("Error making connection with DB", error);
-  }
-};
-
-start();
+  start();
